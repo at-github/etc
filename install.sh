73222bcae79cf88b6a3e66290991271c2771341e
Tarik <tarik.amar@gmail.com>
2014-02-01 19:05:46 +0100

    Make symbolic link instead copies
    More confortable to edit in a git folder

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-echo "start to install, just copying config files"
-declare -a files=(.bash_aliases .bash_mancolor .bash_profile .bash_prompt .bashrc .dircolors .gitconfig .gitignore .tmux.conf);
+echo "start to install, make symbolic link config files"
+declare -a files=('.bash_aliases' '.bash_mancolor' '.bash_profile' '.bash_prompt' '.bashrc' '.dircolors' '.gitconfig' '.gitignore' '.tmux.conf');
 
-cp -i ${files[@]} $HOME
+for file in "${files[@]}"
+do
+    ln -sfb "${PWD}"'/'"${file}" $HOME/"${file}"
+done
 
 source $HOME/.bashrc
