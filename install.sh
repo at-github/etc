b19cca2d708e861f11ac287645daebd801381303
Tarik Amar <tarik.amar@gmail.com>
2015-11-13 07:07:21 +0100

    install - update module

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ source $HOME/.bashrc
 echo "_| Would you like to delete some backupped files in your home ? |_"
 echo "_| (This script make some backupped files if it find file already exist) |_"
 read -p "_| Yes or No (y|n) |_ ??? : " answer
-# TODO if file was found
+
 if [[ "${answer}" == 'y' || "${answer}" == 'yes' ]]; then
     echo 'do it'
     rm -f ${HOME}/.*~
@@ -44,5 +44,8 @@ if [[ "${answer}" == 'y' || "${answer}" == 'yes' ]]; then
 else
     echo "Bye!"
 fi
+
+git submodule init
+git submodule update
 
 cd -
