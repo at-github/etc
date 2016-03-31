4f180385d011d9836b4ed2e69ea849f4e4113083
Tarik Amar <tarik.amar@gmail.com>
2016-03-31 13:55:00 +0200

    install - remove dead code
    I do not use backup option from a will
    so i delete the extra cleanup option

--- install.sh
+++ install.sh
@@ -33,18 +33,6 @@ done
 printf "\n_| Source .bashrc |_\n\n"
 source $HOME/.bashrc
 
-echo "_| Would you like to delete some backupped files in your home ? |_"
-echo "_| (This script make some backupped files if it find file already exist) |_"
-read -p "_| Yes or No (y|n) |_ ??? : " answer
-
-if [[ "${answer}" == 'y' || "${answer}" == 'yes' ]]; then
-    echo 'do it'
-    rm -f ${HOME}/.*~
-    rm -f ${HOME}/'.i3/'*~
-else
-    echo "Bye!"
-fi
-
 git submodule init
 git submodule update
 
